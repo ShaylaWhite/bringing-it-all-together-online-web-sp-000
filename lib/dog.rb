c558b1bc0ec9499d49820d03e4322880438d1da9
Shayla White <shayladwhite1@gmail.com>
2020-01-20 03:18:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,14 @@ class Dog
   end
   
   def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 end
